8eda27f6089af8eb745af257d7a8e338b35f89b3
Hilton Muccillo <hmuccillo@gmail.com>
2020-09-01 13:32:28 -0300

    Publicação sehunda versão

--- custom.js
+++ custom.js
@@ -1,84 +1,288 @@
-//Empresa Início
+// Typed CSS3 com setTimeout jQuery
 $(function () {
-    function typed() {
+    setInterval(function typed() {
+
+        // Cor Verde
+
         // Empresa 1
         setTimeout (function () {
-            $("body > div").prepend("<div class='typewriter-to-right'>Atuei na Performance CG.</div>");               
+            $("#typed").prepend("<span class='typed1'>Já <span>atuei</span> na Performance CG.</span>");               
+            $("#typed > .typed1 > span").css("color", "#4caf50");               
         }, 0)            
         setTimeout (function () {
-            $("body > div > div.typewriter-to-right").remove();
-            $("body > div").prepend("<div class='typewriter-to-left'>Atuei na Performance CG.</div>");               
+            $("#typed > span.typed1").remove();
+            $("#typed").prepend("<span class='typed2'>Já <span>atuei</span> na Performance CG.</span>");
+            $("#typed > .typed2 > span").css("color", "#4caf50");               
         }, 5000)            
         setTimeout (function () {
-            $("body > div > div.typewriter-to-left").remove();                                    
-        }, 6900)
+            $("#typed > span.typed2").remove();                                    
+        }, 6000)
         // Empresa 2
         setTimeout (function () {
-            $("body > div").prepend("<div class='typewriter-to-right'>Atuei na Menes Learninsight.</div>");               
-        }, 7000)            
+            $("#typed").prepend("<span class='typed1'>Já <span>atuei</span> na Menes Learninsight.</span>");
+            $("#typed > .typed1 > span").css("color", "#4caf50");               
+        }, 6000)            
         setTimeout (function () {
-            $("body > div > div.typewriter-to-right").remove();
-            $("body > div").prepend("<div class='typewriter-to-left'>Atuei na Menes Learninsight.</div>");               
-        }, 12000)            
+            $("#typed > span.typed1").remove();
+            $("#typed").prepend("<span class='typed2'>Já <span>atuei</span> na Menes Learninsight.</span>");
+            $("#typed > .typed2 > span").css("color", "#4caf50");                
+        }, 11000)            
         setTimeout (function () {
-            $("body > div > div.typewriter-to-left").remove();                                                        
-        }, 13900)
+            $("#typed > span.typed2").remove();                                                        
+        }, 12000)
         // Empresa 3
         setTimeout (function () {
-            $("body > div").prepend("<div class='typewriter-to-right'>Atuei na Easy System.</div>");               
-        }, 14000)            
+            $("#typed").prepend("<span class='typed1'>Já <span>atuei</span> na Easy System.</span>");
+            $("#typed > .typed1 > span").css("color", "#4caf50");               
+        }, 12000)            
         setTimeout (function () {
-            $("body > div > div.typewriter-to-right").remove();
-            $("body > div").prepend("<div class='typewriter-to-left'>Atuei na Easy System.</div>");               
-        }, 19000)            
+            $("#typed > span.typed1").remove();
+            $("#typed").prepend("<span class='typed2'>Já <span>atuei</span> na Easy System.</span>");
+            $("#typed > .typed2 > span").css("color", "#4caf50");                
+        }, 17000)            
         setTimeout (function () {
-            $("body > div > div.typewriter-to-left").remove();                                                        
-        }, 20900)
+            $("#typed > span.typed2").remove();                                                        
+        }, 18000)
         // Empresa 4
         setTimeout (function () {
-            $("body > div").prepend("<div class='typewriter-to-right'>Atuei no Grupo Tv1.</div>");               
-        }, 21000)            
+            $("#typed").prepend("<span class='typed1'>Já <span>atuei</span> no Grupo Tv1.</span>");
+            $("#typed > .typed1 > span").css("color", "#4caf50");               
+        }, 18000)            
         setTimeout (function () {
-            $("body > div > div.typewriter-to-right").remove();
-            $("body > div").prepend("<div class='typewriter-to-left'>Atuei no Grupo Tv1.</div>");               
-        }, 26000)            
+            $("#typed > span.typed1").remove();
+            $("#typed").prepend("<span class='typed2'>Já <span>atuei</span> no Grupo Tv1.</span>");
+            $("#typed > .typed2 > span").css("color", "#4caf50");                
+        }, 23000)            
         setTimeout (function () {
-            $("body > div > div.typewriter-to-left").remove();                                                                          
-        }, 27900)
+            $("#typed > span.typed2").remove();                                                                          
+        }, 24000)
         // Empresa 5
         setTimeout (function () {
-            $("body > div").prepend("<div class='typewriter-to-right'>Atuei na Wunderman Brasil.</div>");               
-        }, 28000)            
+            $("#typed").prepend("<span class='typed1'>Já <span>atuei</span> na Wunderman Brasil.</span>");
+            $("#typed > .typed1 > span").css("color", "#4caf50");               
+        }, 24000)            
         setTimeout (function () {
-            $("body > div > div.typewriter-to-right").remove();
-            $("body > div").prepend("<div class='typewriter-to-left'>Atuei na Wunderman Brasil.</div>");               
-        }, 33000)            
+            $("#typed > span.typed1").remove();
+            $("#typed").prepend("<span class='typed2'>Já <span>atuei</span> na Wunderman Brasil.</span>");
+            $("#typed > .typed2 > span").css("color", "#4caf50");                
+        }, 29000)            
         setTimeout (function () {
-            $("body > div > div.typewriter-to-left").remove();                                                                          
-        }, 34900)
+            $("#typed > span.typed2").remove();                                                                          
+        }, 30000)
         // Empresa 6
         setTimeout (function () {
-            $("body > div").prepend("<div class='typewriter-to-right'>Atuei na Agência Moustache.</div>");               
+            $("#typed").prepend("<span class='typed1'>Já <span>atuei</span> na Agência Moustache.</span>");
+            $("#typed > .typed1 > span").css("color", "#4caf50");               
+        }, 30000)            
+        setTimeout (function () {
+            $("#typed > span.typed1").remove();
+            $("#typed").prepend("<span class='typed2'>Já <span>atuei</span> na Agência Moustache.</span>");
+            $("#typed > .typed2 > span").css("color", "#4caf50");                
         }, 35000)            
         setTimeout (function () {
-            $("body > div > div.typewriter-to-right").remove();
-            $("body > div").prepend("<div class='typewriter-to-left'>Atuei na Agência Moustache.</div>");               
-        }, 40000)            
-        setTimeout (function () {
-            $("body > div > div.typewriter-to-left").remove();                                                                          
-        }, 41900)
+            $("#typed > span.typed2").remove();                                                                          
+        }, 36000)
         // Empresa 7
         setTimeout (function () {
-            $("body > div").prepend("<div class='typewriter-to-right'>Atuei no Olhar Digital.</div>");               
+            $("#typed").prepend("<span class='typed1'>Já <span>atuei</span> no Olhar Digital.</span>");
+            $("#typed > .typed1 > span").css("color", "#4caf50");               
+        }, 36000)            
+        setTimeout (function () {
+            $("#typed > span.typed1").remove();
+            $("#typed").prepend("<span class='typed2'>Já <span>atuei</span> no Olhar Digital.</span>");
+            $("#typed > .typed2 > span").css("color", "#4caf50");                
+        }, 41000)            
+        setTimeout (function () {
+            $("#typed > span.typed2").remove();                                                                                         
+        }, 42000)
+
+        // Cor Amarelo
+
+        // Empresa 1
+        setTimeout (function () {
+            $("#typed").prepend("<span class='typed1'>Já <span>atuei</span> na Performance CG.</span>");               
+            $("#typed > .typed1 > span").css("color", "#cddc39");               
         }, 42000)            
         setTimeout (function () {
-            $("body > div > div.typewriter-to-right").remove();
-            $("body > div").prepend("<div class='typewriter-to-left'>Atuei no Olhar Digital.</div>");               
+            $("#typed > span.typed1").remove();
+            $("#typed").prepend("<span class='typed2'>Já <span>atuei</span> na Performance CG.</span>");
+            $("#typed > .typed2 > span").css("color", "#cddc39");               
         }, 47000)            
         setTimeout (function () {
-            $("body > div > div.typewriter-to-left").remove();
-            typed();                                                                          
-        }, 48900)
-    }
-    typed();
+            $("#typed > span.typed2").remove();                                    
+        }, 48000)
+        // Empresa 2
+        setTimeout (function () {
+            $("#typed").prepend("<span class='typed1'>Já <span>atuei</span> na Menes Learninsight.</span>");
+            $("#typed > .typed1 > span").css("color", "#cddc39");               
+        }, 48000)            
+        setTimeout (function () {
+            $("#typed > span.typed1").remove();
+            $("#typed").prepend("<span class='typed2'>Já <span>atuei</span> na Menes Learninsight.</span>");
+            $("#typed > .typed2 > span").css("color", "#cddc39");                
+        }, 53000)            
+        setTimeout (function () {
+            $("#typed > span.typed2").remove();                                                        
+        }, 54000)
+        // Empresa 3
+        setTimeout (function () {
+            $("#typed").prepend("<span class='typed1'>Já <span>atuei</span> na Easy System.</span>");
+            $("#typed > .typed1 > span").css("color", "#cddc39");               
+        }, 54000)            
+        setTimeout (function () {
+            $("#typed > span.typed1").remove();
+            $("#typed").prepend("<span class='typed2'>Já <span>atuei</span> na Easy System.</span>");
+            $("#typed > .typed2 > span").css("color", "#cddc39");                
+        }, 59000)            
+        setTimeout (function () {
+            $("#typed > span.typed2").remove();                                                        
+        }, 60000)
+        // Empresa 4
+        setTimeout (function () {
+            $("#typed").prepend("<span class='typed1'>Já <span>atuei</span> no Grupo Tv1.</span>");
+            $("#typed > .typed1 > span").css("color", "#cddc39");               
+        }, 60000)            
+        setTimeout (function () {
+            $("#typed > span.typed1").remove();
+            $("#typed").prepend("<span class='typed2'>Já <span>atuei</span> no Grupo Tv1.</span>");
+            $("#typed > .typed2 > span").css("color", "#cddc39");                
+        }, 65000)            
+        setTimeout (function () {
+            $("#typed > span.typed2").remove();                                                                          
+        }, 66000)
+        // Empresa 5
+        setTimeout (function () {
+            $("#typed").prepend("<span class='typed1'>Já <span>atuei</span> na Wunderman Brasil.</span>");
+            $("#typed > .typed1 > span").css("color", "#cddc39");               
+        }, 66000)            
+        setTimeout (function () {
+            $("#typed > span.typed1").remove();
+            $("#typed").prepend("<span class='typed2'>Já <span>atuei</span> na Wunderman Brasil.</span>");
+            $("#typed > .typed2 > span").css("color", "#cddc39");                
+        }, 71000)            
+        setTimeout (function () {
+            $("#typed > span.typed2").remove();                                                                          
+        }, 72000)
+        // Empresa 6
+        setTimeout (function () {
+            $("#typed").prepend("<span class='typed1'>Já <span>atuei</span> na Agência Moustache.</span>");
+            $("#typed > .typed1 > span").css("color", "#cddc39");               
+        }, 72000)            
+        setTimeout (function () {
+            $("#typed > span.typed1").remove();
+            $("#typed").prepend("<span class='typed2'>Já <span>atuei</span> na Agência Moustache.</span>");
+            $("#typed > .typed2 > span").css("color", "#cddc39");                
+        }, 77000)            
+        setTimeout (function () {
+            $("#typed > span.typed2").remove();                                                                          
+        }, 78000)
+        // Empresa 7
+        setTimeout (function () {
+            $("#typed").prepend("<span class='typed1'>Já <span>atuei</span> no Olhar Digital.</span>");
+            $("#typed > .typed1 > span").css("color", "#cddc39");               
+        }, 78000)            
+        setTimeout (function () {
+            $("#typed > span.typed1").remove();
+            $("#typed").prepend("<span class='typed2'>Já <span>atuei</span> no Olhar Digital.</span>");
+            $("#typed > .typed2 > span").css("color", "#cddc39");                
+        }, 83000)            
+        setTimeout (function () {
+            $("#typed > span.typed2").remove();                                                                                         
+        }, 84000)
+
+         // Cor Azul
+
+        // Empresa 1
+        setTimeout (function () {
+            $("#typed").prepend("<span class='typed1'>Já <span>atuei</span> na Performance CG.</span>");               
+            $("#typed > .typed1 > span").css("color", "#03A9F4");               
+        }, 84000)            
+        setTimeout (function () {
+            $("#typed > span.typed1").remove();
+            $("#typed").prepend("<span class='typed2'>Já <span>atuei</span> na Performance CG.</span>");
+            $("#typed > .typed2 > span").css("color", "#03A9F4");               
+        }, 89000)            
+        setTimeout (function () {
+            $("#typed > span.typed2").remove();                                    
+        }, 90000)
+        // Empresa 2
+        setTimeout (function () {
+            $("#typed").prepend("<span class='typed1'>Já <span>atuei</span> na Menes Learninsight.</span>");
+            $("#typed > .typed1 > span").css("color", "#03A9F4");               
+        }, 90000)            
+        setTimeout (function () {
+            $("#typed > span.typed1").remove();
+            $("#typed").prepend("<span class='typed2'>Já <span>atuei</span> na Menes Learninsight.</span>");
+            $("#typed > .typed2 > span").css("color", "#03A9F4");                
+        }, 95000)            
+        setTimeout (function () {
+            $("#typed > span.typed2").remove();                                                        
+        }, 96000)
+        // Empresa 3
+        setTimeout (function () {
+            $("#typed").prepend("<span class='typed1'>Já <span>atuei</span> na Easy System.</span>");
+            $("#typed > .typed1 > span").css("color", "#03A9F4");               
+        }, 96000)            
+        setTimeout (function () {
+            $("#typed > span.typed1").remove();
+            $("#typed").prepend("<span class='typed2'>Já <span>atuei</span> na Easy System.</span>");
+            $("#typed > .typed2 > span").css("color", "#03A9F4");                
+        }, 101000)            
+        setTimeout (function () {
+            $("#typed > span.typed2").remove();                                                        
+        }, 102000)
+        // Empresa 4
+        setTimeout (function () {
+            $("#typed").prepend("<span class='typed1'>Já <span>atuei</span> no Grupo Tv1.</span>");
+            $("#typed > .typed1 > span").css("color", "#03A9F4");               
+        }, 102000)            
+        setTimeout (function () {
+            $("#typed > span.typed1").remove();
+            $("#typed").prepend("<span class='typed2'>Já <span>atuei</span> no Grupo Tv1.</span>");
+            $("#typed > .typed2 > span").css("color", "#03A9F4");                
+        }, 107000)            
+        setTimeout (function () {
+            $("#typed > span.typed2").remove();                                                                          
+        }, 108000)
+        // Empresa 5
+        setTimeout (function () {
+            $("#typed").prepend("<span class='typed1'>Já <span>atuei</span> na Wunderman Brasil.</span>");
+            $("#typed > .typed1 > span").css("color", "#03A9F4");               
+        }, 108000)            
+        setTimeout (function () {
+            $("#typed > span.typed1").remove();
+            $("#typed").prepend("<span class='typed2'>Já <span>atuei</span> na Wunderman Brasil.</span>");
+            $("#typed > .typed2 > span").css("color", "#03A9F4");                
+        }, 113000)            
+        setTimeout (function () {
+            $("#typed > span.typed2").remove();                                                                          
+        }, 114000)
+        // Empresa 6
+        setTimeout (function () {
+            $("#typed").prepend("<span class='typed1'>Já <span>atuei</span> na Agência Moustache.</span>");
+            $("#typed > .typed1 > span").css("color", "#03A9F4");               
+        }, 114000)            
+        setTimeout (function () {
+            $("#typed > span.typed1").remove();
+            $("#typed").prepend("<span class='typed2'>Já <span>atuei</span> na Agência Moustache.</span>");
+            $("#typed > .typed2 > span").css("color", "#03A9F4");                
+        }, 119000)            
+        setTimeout (function () {
+            $("#typed > span.typed2").remove();                                                                          
+        }, 120000)
+        // Empresa 7
+        setTimeout (function () {
+            $("#typed").prepend("<span class='typed1'>Já <span>atuei</span> no Olhar Digital.</span>");
+            $("#typed > .typed1 > span").css("color", "#03A9F4");               
+        }, 120000)            
+        setTimeout (function () {
+            $("#typed > span.typed1").remove();
+            $("#typed").prepend("<span class='typed2'>Já <span>atuei</span> no Olhar Digital.</span>");
+            $("#typed > .typed2 > span").css("color", "#03A9F4");                
+        }, 125000)            
+        setTimeout (function () {
+            $("#typed > span.typed2").remove();                                                                                         
+        }, 126000)
+        return typed;
+    }(), 126000);
 })
